b261423b348536d3e6c661007fedaf136cad461a
itallopacheco <itallopss@dcomp.ufs.br>
2024-02-14 00:10:42 -0300

    Exemplo de CRUD

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,12 +1,60 @@
-from fastapi.testclient import TestClient
-
-from fast_zero.app import app
-
-
-def test_root_deve_retornar_200_e_hello_world():
-    client = TestClient(app)
-
+def test_root_deve_retornar_200_e_hello_world(client):
     response = client.get('/')
 
     assert response.status_code == 200
-    assert response.json() == {'message': 'Hello Worldgh'}
+    assert response.json() == {'message': 'Hello World'}
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'alice',
+            'email': 'alice@example.com',
+            'password': 'secret',
+        },
+    )
+    assert response.status_code == 201
+    assert response.json() == {
+        'username': 'alice',
+        'email': 'alice@example.com',
+        'id': 1,
+    }
+
+
+def test_read_users(client):
+    response = client.get('/users/')
+    assert response.status_code == 200
+    assert response.json() == {
+        'users': [
+            {
+                'username': 'alice',
+                'email': 'alice@example.com',
+                'id': 1,
+            }
+        ]
+    }
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'itallo',
+            'email': 'itallo@example.com',
+            'password': 'newpassword',
+        },
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+        'username': 'itallo',
+        'email': 'itallo@example.com',
+        'id': 1,
+    }
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.status_code == 200
+    assert response.json() == {'message': 'User deleted'}
